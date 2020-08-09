a91c8e7e17ecdc166c01096f4afc1ac992969941
sinankaradavut <69120351+sinankaradavut@users.noreply.github.com>
2020-08-09 12:07:10 -0400

    Update FirstClass.java

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -10,7 +10,7 @@ public class FirstClass {
 	         
 		
 		
-		System.out.println("This is the second commit from remote master");
+		System.out.println("This is the second commit from remote master1");
 	}
 
 }
